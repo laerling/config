df7c6217401ef480df2704de8d73c306b0efe240
laerling <laerling@posteo.de>
2019-08-27 23:46:33 +0200

    holodeck-laptop: Set wallpaper on external screen only
    Also:
    - Fix: External screen not primary

--- holodeck-laptop/xinitrc.pl
+++ holodeck-laptop/xinitrc.pl
@@ -5,7 +5,9 @@ use warnings;
 while(<>){
 	if($_ =~ /^#own configuration/){
 		print;
-		print "xrandr --auto --output LVDS-1 --below VGA-1\n"; #external screen (e. g. a projector), if available, over regular screen
+		print "xrandr --auto --output VGA-1 --primary --above LVDS-1\n"; # external screen (e. g. a projector), if available, over regular screen
+		print "externalscreen=\$(xrandr | grep 'VGA-1 connected')\n";
+		print "if [ -n \"\$externalscreen\" ]; then feh --bg-fill --xinerama-index 0 /home/laerling/.wallpaper; fi\n"; # show wallpaper on external screen only
 	}else{
 		# print the rest
 		print
