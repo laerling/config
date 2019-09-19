0bc5abad60b3a19f72b5713d37f626a7a67768de
laerling <laerling@posteo.de>
2019-09-19 13:25:55 +0200

    hologram-gui: Use guake instead of gnome-terminal

--- hologram-gui/xinitrc.pl
+++ hologram-gui/xinitrc.pl
@@ -7,9 +7,10 @@ while(<>){
 	print "#own configuration"; #keep this for other holograms!
 	# Monitor config belongs in holodecks, since it's machine-dependent
 	print "\n[[ -f /etc/X11/xinit/Xresources ]] && xrdb -merge /etc/X11/xinit/Xresources"; #load global Xresources provided by this hologram
+	print "\nguake &";
 	print "\nexec i3";
 	exit;
     }else{
-	print
+	print;
     }
 }
