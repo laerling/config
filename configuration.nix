483472c396bfc271911fd027583026d6db2ce1a1
laerling <laerling@posteo.de>
2020-10-27 18:24:40 +0100

    nix: Add aliases and colors for less/man/...

--- configuration.nix
+++ configuration.nix
@@ -51,6 +51,23 @@ with builtins; {
     extraGroups = [ "wheel" ];
   };
 
+  # shell
+  programs.bash.interactiveShellInit = ''
+    # Add colors to less/man/...
+    export LESS_TERMCAP_mb=$'\E[1;31m'     # begin bold
+    export LESS_TERMCAP_md=$'\E[1;36m'     # begin blink
+    export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+    export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+    export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+    export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+    export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+    export GROFF_NO_SGR=1                  # for konsole and gnome-terminal
+
+    alias loop='mpv --loop-playlist'
+    alias play='loop --shuffle'
+    alias music='play --no-video ~/Musik/'
+  '';
+
   # packages
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; let
