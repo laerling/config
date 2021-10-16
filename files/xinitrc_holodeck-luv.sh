2c59ecde6c8315a37deaa85f767ae7880fc24043
laerling <laerling@posteo.de>
2021-10-16 23:17:07 +0200

    holodeck-luv: Show wallpaper on all screens or only on external

--- files/xinitrc_holodeck-luv.sh
+++ files/xinitrc_holodeck-luv.sh
@@ -1,7 +1,16 @@
 xrandr --auto --output VGA-1 --primary --left-of LVDS-1 # external screen (e. g. a projector), if available, over regular screen
-externalscreen=$(xrandr | grep 'VGA-1 connected')
 
-# show wallpaper on external screen only
-if [ -n "$externalscreen" ]; then
-	feh --bg-fill --xinerama-index 0 /home/laerling/.wallpaper
+wallpaper_path=/home/laerling/.wallpaper
+
+wp="$(realpath "$wallpaper_path")"
+if [ -e "$wp" ]; then
+  wallpaper_on_external_screen_only=''
+  if [ -n "$wallpaper_on_external_screen_only" ]; then
+    externalscreen=$(xrandr | grep 'VGA-1 connected')
+    if [ -n "$externalscreen" ]; then
+      feh --bg-fill --xinerama-index 0 "$wallpapaer_path"
+    fi
+  else
+    feh --bg-fill "$wp"
+  fi
 fi
