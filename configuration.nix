3b191a602fff84e97a9768467ce3d5e1fd7f3823
laerling <laerling@posteo.de>
2022-02-19 01:19:01 +0100

    configuration.nix: Add package borgbackup

--- configuration.nix
+++ configuration.nix
@@ -66,7 +66,7 @@
       allPkgs = pkgs // ownPkgs;
     in
       with allPkgs; let
-        base       = [ file git gnumake screen tree unzip vim wget ];
+        base       = [ borgbackup file git gnumake screen tree unzip vim wget ];
         # breeze-icons contains icons for kolourpaint
         gui_base   = [ breeze-icons firefox gnome3.gnome-tweaks kolourpaint pavucontrol source-code-pro ];
         gui_ubuntu = [ gnomeExtensions.dash-to-dock ubuntu_font_family ubuntu-themes yaru-theme ];
