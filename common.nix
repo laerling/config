60d02685a18177ce34d96de1bc14d744b54b89f3
laerling <laerling@posteo.de>
2025-07-20 16:50:18 +0200

    choices += ollama

--- common.nix
+++ common.nix
@@ -301,6 +301,7 @@ rec {
     chosenPackages = let packageForChoice = with pkgs; {
       minecraft = prismlauncher;
       crt = cool-retro-term;
+      inherit ollama;
     }; in with builtins; concatMap
     (c: if choose c then [packageForChoice."${c}"] else [])
     (attrNames packageForChoice);
